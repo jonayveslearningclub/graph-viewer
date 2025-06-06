43e1c0c61e56a7d8c3c36fe2dcfaf887e74e8d36
jonayveslearningclub <info@jonayveslearningclub.com>
2025-06-06 12:37:04 -0400

    Add new geometry graph

---
+++ graph-library/p5-1749227205982.js
@@ -0,0 +1,27 @@
+{
+  "data": [
+    {
+      "type": "pie",
+      "labels": ["Red", "Blue", "Green", "Yellow", "Purple"],
+      "values": [3, 4, 2, 1, 2],
+      "marker": {
+        "colors": ["red", "blue", "green", "yellow", "purple"]
+      },
+      "textinfo": "label+percent",
+      "textfont": {
+        "size": 14
+      }
+    }
+  ],
+  "layout": {
+    "title": {
+      "text": "What is the chance that the spinner will land on: a) red? b) blue? c) green? d) any color?",
+      "font": {
+        "size": 16
+      }
+    },
+    "width": 800,
+    "height": 600,
+    "showlegend": false
+  }
+}
